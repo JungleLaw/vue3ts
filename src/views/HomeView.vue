89be7c29db7250c2ceb857627e224353e9c0dace
Jungle <Junglelaw@live.com>
2022-09-08 17:21:10 +0800

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<!-- @format -->
+
+<template>
+	<main>
+		<the-welcome></the-welcome>
+	</main>
+</template>
+
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue';
+</script>
---
+++ vite.config.ts
@@ -0,0 +1,37 @@
+/** @format */
+
+import { fileURLToPath, URL } from 'node:url';
+
+import { defineConfig, loadEnv } from 'vite';
+import type { UserConfig, ConfigEnv } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command, mode }: ConfigEnv) => {
+	// console.log('command', command);
+	// console.log('mode', mode);
+	const root = process.cwd();
+	// console.log('process.env', process.env.APP_ENV);
+	// console.log('root', root);
+	const env = loadEnv(mode, root, ''); // 环境变量对象
+	console.log('环境变量------ %s', env.APP_ENV);
+	// console.log('文件路径（ process.cwd()）------', root);
+	// console.log('文件路径（dirname）------', __dirname + '/src');
+	// console.log('import.meta.url', import.meta.url);
+
+	return {
+		plugins: [vue(), vueJsx()],
+		resolve: {
+			alias: {
+				'@': fileURLToPath(new URL('./src', import.meta.url)),
+			},
+		},
+		server: {
+			host: true,
+			port: 1234,
+			https: false,
+			open: false,
+		},
+	};
+});
