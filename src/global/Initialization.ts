a1bf5635899ceb9d8d72f23b6575412486d6b96c
Jungle <junglelaw@live.com>
2022-09-18 12:06:13 +0800

    refactor #1 优化日志功能

--- src/global/Initialization.ts
+++ src/global/Initialization.ts
@@ -2,22 +2,32 @@
  * @ Author: Captain
  * @ Create Time: 2022-09-17 15:55:25
  * @ Modified by: Captain
- * @ Modified time: 2022-09-17 17:57:39
+ * @ Modified time: 2022-09-18 04:53:55
  * @ Description:
  */
 
 import { App } from 'vue';
 import Logger from '@/global/Logger';
 import Theme from '@/global/Theme';
-import Settings from '@/global/settings';
+import Settings from '@/global/Settings';
 
 export default class Initialization {
 	public static setup(app: App) {
 		// process.env.VITE_LOGGER_DEBUG;
+		// console.log(__APP_ENV__);
+		// console.log('a%dbb', 1);
+		// console.log('Tag', process);
+		// console.log('=========');
+
+		Logger.log('a%dbb', 1);
 		Logger.info('Tag', process);
+		Logger.error('Tag', process);
+		Logger.warn('Tag', process);
+		Logger.debug('Tag', process);
+		// Logger.track('Tag', process);
 		Theme.setupTheme();
 		app.config.errorHandler = () => {
-			Logger.info('Tag', 'app error');
+			Logger.log('app error');
 		};
 		app.config.globalProperties.$settings = Settings;
 	}
--- src/global/Logger.ts
+++ src/global/Logger.ts
@@ -2,52 +2,72 @@
  * @ Author: Captain
  * @ Create Time: 2022-09-17 14:50:58
  * @ Modified by: Captain
- * @ Modified time: 2022-09-17 17:56:59
+ * @ Modified time: 2022-09-18 04:41:20
  * @ Description:
  */
-const log = (tag: string = 'Logger', msg: any, ...optionalParams: any[]) => {
-	// tslint:disable-next-line:no-console
-	console.log(`${tag}=>%o`, msg, ...optionalParams);
-};
 
 class Logger {
+	private static readonly drop: boolean = process['DROP_LOG'];
+	// private static readonly drop: boolean = false;
+	private static readonly Type = {
+		Default: { value: 0, color: '#41b883' },
+		Info: { value: 1, color: 'blue' },
+		Warn: { value: 2, color: 'orange' },
+		Error: { value: 3, color: 'red' },
+		Debug: { value: 4, color: '#35495e' },
+		Track: { value: 5, color: '#001528' },
+	};
+	private static print(type: object, tag: string, msg: any, ...optionalParams: any[]) {
+		if (Logger.drop) return;
+
+		// 'abc' + `%o`,
+		let fn: Function;
+		switch (type) {
+			case Logger.Type.Info:
+				fn = console.info;
+				break;
+			case Logger.Type.Warn:
+				fn = console.warn;
+				break;
+			case Logger.Type.Error:
+				fn = console.error;
+				break;
+			case Logger.Type.Debug:
+				fn = console.debug;
+				break;
+			case Logger.Type.Track:
+				fn = console.trace;
+				break;
+			default:
+				fn = console.log;
+		}
+		console.group(`%c${tag}`, `background:${type['color']} ; padding: 2px 10px; border-radius: 3px ;  color: #fff`);
+		fn(msg, ...optionalParams);
+		console.groupEnd();
+	}
 	/**
 	 *
 	 * @param tag
 	 * @param msg
 	 * @param args
 	 */
-	// tag: string = 'Logger',
-	public static info(tag: string, msg: any, ...optionalParams: any[]) {
-		if (this.debug) {
-			// tslint:disable-next-line:no-console
-			log(tag, msg, ...optionalParams);
-		}
+	public static log(msg: any, ...optionalParams: any[]) {
+		Logger.print(Logger.Type.Default, 'Logger', msg, ...optionalParams);
 	}
-	public static setDebug(debug: boolean): void {
-		this.debug = debug;
+	public static info(tag: string = 'Logger info', msg: any, ...optionalParams: any[]) {
+		Logger.print(Logger.Type.Info, tag, msg, ...optionalParams);
 	}
-	private static readonly Type = {
-		Info: 1,
-		Warn: 2,
-		Error: 3,
-		Debug: 4,
-		Track: 5,
-	};
-	private static debug: boolean = process.debug;
-
-	private static convert(msg: any): string {
-		const type: string = typeof msg;
-		switch (type) {
-			case 'string':
-				break;
-			case 'number':
-				break;
-			case 'object':
-				break;
-			default:
-		}
-		return '';
+	public static warn(tag: string = 'Logger warn', msg: any, ...optionalParams: any[]) {
+		Logger.print(Logger.Type.Warn, tag, msg, ...optionalParams);
+	}
+	public static error(tag: string = 'Logger error', msg: any, ...optionalParams: any[]) {
+		Logger.print(Logger.Type.Error, tag, msg, ...optionalParams);
+	}
+	public static debug(tag: string = 'Logger debug', msg: any, ...optionalParams: any[]) {
+		Logger.print(Logger.Type.Debug, tag, msg, ...optionalParams);
+	}
+	public static track(tag: string = 'Logger track', msg: any, ...optionalParams: any[]) {
+		Logger.print(Logger.Type.Track, tag, msg, ...optionalParams);
 	}
 }
 
