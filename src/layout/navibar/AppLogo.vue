0696cc44454a06995fb7393d3f7e62236f08915c
Jungle <junglelaw@live.com>
2022-09-20 00:58:08 +0800

    feat #1 监听设置中的主题值

--- src/layout/navibar/AppLogo.vue
+++ src/layout/navibar/AppLogo.vue
@@ -2,28 +2,49 @@
 	<div class="app-logo-root">
 		<!-- <img v-if="!collapse" class="logo-collapse" :src="Logo" alt="hcttop-logo" /> -->
 		<router-link to="/">
-			<!-- <div class="app-logo">
-				<img :src="require(`@/assets/images/navbar/app-logo-${theme}.png`)" alt="hcttop-logo" />
+			<div class="app-logo">
+				<!-- <img :src="require(`@/assets/images/navbar/app-logo-${theme}.png`)" alt="hcttop-logo" />
 				<div :class="['app-logo__title', `app-logo__title--${theme}-theme`]">
-					<span>{{ getProjectTitle }}</span>
-				</div>
-			</div> -->
+				</div> -->
+				<img :src="AppLogo()" alt="hcttop-logo" />
+				<span>{{ settings.projectName }}</span>
+			</div>
 		</router-link>
 	</div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import ClassicLogo from '@/assets/layout/navibar/app-logo-classic.png';
+
+import Logger from '@/global/Logger';
+import { defineComponent, reactive } from 'vue';
 
 export default defineComponent({
 	name: 'AppLogo',
 	data() {
 		return {
-			settings: this.$settings,
+			ClassicLogo,
+			settings: reactive(this.$settings),
 		};
 	},
 	mounted() {
 		// console.log(this.$settings);
+		Logger.info('this.settings', this.settings);
+	},
+	watch: {
+		settings: {
+			handler(newName, oldName) {
+				Logger.info('newName', newName);
+				Logger.info('oldName', oldName);
+			},
+			immediate: true,
+			deep: true,
+		},
+	},
+	methods: {
+		AppLogo() {
+			return this.ClassicLogo;
+		},
 	},
 });
 </script>
