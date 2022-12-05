78c48da8ca1b86fc32e8cddc357be427239e4377
Jungle <Junglelaw@live.com>
2022-12-05 18:15:01 +0800

    #1 http封装

---
+++ src/commons/http/constants.ts
@@ -0,0 +1,15 @@
+export enum HttpMethod {
+	GET = 'GET',
+	POST = 'POST',
+	PUT = 'PUT',
+	DELETE = 'DELETE',
+}
+
+export enum HttpContentType {
+	// json
+	JSON = 'application/json;charset=UTF-8',
+	// form-data qs
+	FORM_URLENCODED = 'application/x-www-form-urlencoded;charset=UTF-8',
+	// form-data  upload
+	FORM_DATA = 'multipart/form-data;charset=UTF-8',
+}
---
+++ src/commons/http/iRequest.ts
@@ -0,0 +1,9 @@
+export default interface IRequest {
+	post();
+	get();
+	delete();
+	put();
+	download();
+	upload();
+	request();
+}
